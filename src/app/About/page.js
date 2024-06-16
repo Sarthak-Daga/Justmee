659e73cae50802daa7dfc04fd3cb61174629fa63
Sarthak Daga <sarthakdaga21@gmail.com>
2024-06-16 16:00:41 +0530

    about

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <div className="mt-20 ml-16">
+        <div className="w-[50%]  inline-flex">About Us</div>
+        <div className="absolute h-[500px] w-[500px] inline-flex">
+          <Image src="/about.png" fill />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default About
+export default About;
