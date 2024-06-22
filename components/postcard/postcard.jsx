afb7d593f9e33ef950b38d4d1c12b7b000e70839
Sarthak Daga <sarthakdaga21@gmail.com>
2024-06-22 14:22:18 +0530

    post page done

--- components/postcard/postcard.jsx
+++ components/postcard/postcard.jsx
@@ -1,20 +1,31 @@
 import React from "react";
-
-const postcard = () => {
+import Image from "next/image";
+import Link from "next/link";
+const Postcard = () => {
   return (
     <>
-      <div class="m-20 h-[400px] w-[250px] rounded-lg bg-[#0c0c23] flex flex-col gap-[15px] p-2 text-white">
-        <div class="flex gap-0">
-          <div class="bg-blue-200 h-[280px] w-[220px] rounded-lg">img</div>
-          <span class="rotate-90 absolute ml-[194px] mt-[137px]">
+      <div className="h-[400px] w-[250px] rounded-lg flex flex-col gap-[15px] text-white">
+        <div className="flex gap-0">
+          <div className="h-[280px] w-[220px] rounded-lg">
+            <Image
+              src="/hello.png"
+              layout="responsive"
+              width={230}
+              height={300}
+              objectFit="contain"
+              alt="Blog Post Image"
+              className="rounded-lg"
+            />
+          </div>
+          <span className="rotate-90 absolute ml-[194px] mt-[137px]">
             21.11.2005
           </span>
         </div>
         <div>
-          <h1 class="font-bold text-2xl ">Title</h1>
-          <p class="text-white opacity-70">Description</p>
-          <Link>
-            <span class="underline">Read More</span>
+          <h1 className="font-bold text-2xl ">Title</h1>
+          <p className="text-white opacity-70">Description</p>
+          <Link href={"/blog.jsx"}>
+            <span className="underline">Read More</span>
           </Link>
         </div>
       </div>
@@ -22,4 +33,4 @@ const postcard = () => {
   );
 };
 
-export default postcard;
+export default Postcard;
